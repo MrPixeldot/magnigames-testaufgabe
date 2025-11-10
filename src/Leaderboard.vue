152de61192e727a24fed317e23b3c2bb044070e7
MrPixeldot <jakob.simkomat@icloud.com>
2025-11-10 21:53:36 +0100

    learned about buttons, methods and displaying or hiding templates

--- src/Leaderboard.vue
+++ src/Leaderboard.vue
@@ -1,8 +1,12 @@
 <template>
-  <div>
-    <p>{{ player }} - {{ score }}</p>
-    <button @click="score++">Increase score</button>
-  </div>
+    <button @click="toggleShow">
+        <span v-if="!show">Show stats</span>
+        <span v-else>Hide stats</span>
+    </button>
+    <div v-show="show">
+        <p>{{ player }} || {{ score }}</p>
+        <button @click="score++">Increase score</button>
+        <button @click="score--">Decrease score</button></div>
 </template>
 
 <script>
@@ -10,8 +14,14 @@ export default {
   data() {
     return {
       player: 'Martin Siebert',
-      score: 0
+      score: 0,
+      show: false
     }
-  }
+},
+methods: {
+    toggleShow () {
+        this.show = !this.show
+    }
+    }
 }
 </script>
