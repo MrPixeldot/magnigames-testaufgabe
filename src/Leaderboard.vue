c577abf3ee836ce28b9cbfb116d55e1f45268a85
MrPixeldot <jakob.simkomat@icloud.com>
2025-11-11 22:12:33 +0100

    made basic player list with a wappen png and randoml generated score

--- src/Leaderboard.vue
+++ src/Leaderboard.vue
@@ -1,25 +1,30 @@
 <template>
-    <ul>
+    <ul style="padding-left: 0">
         <li v-for="player in players" class="wappen">
             <img :src="player.wappen" alt="player.name">
-            <span class="text">{{ player.name }} - {{ player.city}}<div v-show="show"> {{ player.score }}</div></span>
+            <span class="text">{{ player.name }} - {{ player.city}}  <span v-show="show">- {{ player.score }}</span></span>
         </li>
     </ul>
-  <button @click="toggleShow">
+  <button @click="toggleShow" class="button">
     <span v-if="show">Show score</span>
     <span v-else>Hide score</span>
-  </button>  
+  </button>
+  <button @click="regenerateScore" class="button">Regenerate score</button>  
 </template>
 
 <script>
+function randomScore(max) {
+      return Math.floor(Math.random() * max);
+}
+
 export default {
   data() {
     return {
       players: [
-        {name: 'Martin Siebert', city: 'Braunschweig', wappen: '/Wappen1.png'},
-        {name: 'Jakob Simko', city: 'Salzburg', wappen: '/Wappen2.png'},
-        {name: 'Eric Biering', city: 'Broitzem', wappen: '/Wappen3.png'},
-        {name: 'Finn-Rico Burghardt', city: "Weststadt", wappen: '/Wappen4.png'}
+        {name: 'Martin Siebert', city: 'Braunschweig', wappen: '/Wappen1.png', score: randomScore(1111)},
+        {name: 'Jakob Simko', city: 'Salzburg', wappen: '/Wappen2.png', score: randomScore(1111)},
+        {name: 'Eric Biering', city: 'Broitzem', wappen: '/Wappen3.png', score: randomScore(1111)},
+        {name: 'Finn-Rico Burghardt', city: "Weststadt", wappen: '/Wappen4.png', score: randomScore(1111)}
       ],
       show: false,
     }
@@ -28,13 +33,16 @@ methods: {
     toggleShow() {
         this.show = !this.show
     },
+    regenerateScore() {
+    this.players.forEach(player => {player.score = randomScore(1111)});
     }
-}    
+} 
+}  
 </script>
 
 <style>
 .button {
- margin: 2;
+ margin: 2px;
  font-family: monospace;
 }
 .text {
@@ -45,6 +53,5 @@ methods: {
   display: flex;
   align-items: center;
   margin-bottom: 4px; 
-  list-style: none;
 } 
 </style>
